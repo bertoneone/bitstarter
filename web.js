16684b12497083938f84f540ff6932ef20edf569
bertoneone <berto20@fastwebnet.it>
2013-07-10 23:56:57 +0000

    debug5

--- web.js
+++ web.js
@@ -4,12 +4,12 @@ var app = express.createServer(express.logger());
 
 var infile = "index.html";
 
-var rdfile = fs.readFileSync(infile, 'utf-8');
+//var rdfile =
 
 //var out = buf.toString('utf-8', 0, 27);
 
 app.get('/', function(request, response) {
-  response.send(rdfile);
+  response.send(fs.readFileSync(infile, 'utf-8'));
 });
 
 var port = process.env.PORT || 5000;
