650a6081a56aff0bf9177669a8b668e4caebf874
bertoneone <berto20@fastwebnet.it>
2013-07-11 00:16:26 +0000

    polished

--- web.js
+++ web.js
@@ -6,12 +6,8 @@ var app = express.createServer(express.logger());
 
 var infile = 'index.html';
 
-//var buf = fs.readFileSync(infile);
-
-//var out = buf.toString('utf-8', 0, 27);
-
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(infile, 'utf-8'));
+  response.send(fs.readFileSync(infile).toString());
 });
 
 var port = process.env.PORT || 5000;
