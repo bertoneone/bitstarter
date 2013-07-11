1fb9d9f7bd06e2892638e00e75c306f7dade3f10
bertoneone <berto20@fastwebnet.it>
2013-07-11 00:03:35 +0000

    debug6

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 var infile = "index.html";
 
-//var rdfile =
+//var buf = fs.readFileSync(infile);
 
-//var out = buf.toString('utf-8', 0, 27);
+var out = buf.toString('utf-8', 0, 27);
 
 app.get('/', function(request, response) {
   response.send(fs.readFileSync(infile, 'utf-8'));
